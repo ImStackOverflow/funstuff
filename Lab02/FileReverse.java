4fe2588d410f77fa014a4fdbb84f9e40e6439a05
Gavin chen <ghchen@ucsc.edu>
2017-01-25 00:09:06 -0800

    did word reversal functions

--- Lab02/FileReverse.java
+++ Lab02/FileReverse.java
@@ -4,16 +4,30 @@ import java.util.Scanner;
 
 class FileReverse{
 	 public static void main(String[] args){
-	 	String output = stringReverse("eat my big penis");
-	 	System.out.println(output);
+	 	String ass = "eat my big penis";
+	 	String[] out = Reverse(ass);
+	 	for (int i=0; i<=out.length-1; i++){
+	 		System.out.println(out[i]);
+	 	}
+	}
 
+	public static String[] Reverse(String s){//reverses words
+		String[] shit = s.split(" ");//parses words
+	 	String[] back = new String[shit.length];
+	 	for (int i = 0; i<=shit.length-1;i++){//reverses words
+	 		back[i] = wordReverse(shit[i]);
+	 	}
+	 	return back;//returns reversed words in order they were passed
 
 	}
-	public static String stringReverse(String s){
-	char input[s.length]=s.split("");
-	char[] output = new char[s.length];
-	for (int i = s.length; i>= 0; i--){
-		output[s.length-i]=input[i];
+	public static String wordReverse(String word){//reverses words
+	String[] input=word.split("");//parses characters
+	StringBuilder output = new StringBuilder();
+	int tail = input.length-1;//last element of word array
+	for (int i = tail; i!=0; i--){
+		output.append(input[i]);//store leters starting from last
 	}
+	return output.toString();//return reversed word
 	}
-}
+	
+	}
