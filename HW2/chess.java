69dd5515a75833196830d2e84657601a7a0eabfd
Gavin chen <ghchen@ucsc.edu>
2017-02-01 20:22:10 -0800

    SO FUCKING CLOSE

--- HW2/chess.java
+++ HW2/chess.java
@@ -41,13 +41,11 @@ class Node {
 			else return null;//not able to splice in node
 	}
 	static Node clearList(Node clear){//clears list starting from top, returns last node
-		clear = clear.head;
-		while (clear.after != null){
-			System.out.println("the head node is: "+clear.head.piece);
-			clear = deleteNode(clear);
-			
+		if (clear.after != null){
+			clearList(clear.after);
 		}
-		return clear;
+		if (clear != clear.head) deleteNode(clear); //delete up to head
+		return clear;//return head
 	}
 }
 class chess extends Node{
@@ -56,120 +54,57 @@ class chess extends Node{
 		Scanner in = new Scanner(new File(args[0]));//in file
 		PrintWriter out = new PrintWriter(new File(args[1]));//out file
 		Node poop = new Node();//initiate linked list
+		Node helper = new Node();
+		boolean found = false;
 		poop.head = poop;
-		String[] find = new String[2];
+		String[] look = new String[2];
 		String input;
 		int i = 0;
 		while (in.hasNextLine()){
-			System.out.println(i++);
 			input = in.nextLine();//get board data line
+			poop = poop.head;
 			poop = clearList(poop);
 			poop = createBoard(input, poop);//make linked list of pieces
-/*
-			
-			System.out.println("the board is: "+verify(poop));
-			poop = poop.head;
-			while (poop!=null){
-			System.out.print("Bsize: "+poop.piece.boardSize);
-			System.out.print("  (x,y): "+(poop.piece.X)+","+(poop.piece.Y));
-			System.out.println("  piece: "+poop.piece);
-			poop = poop.after;
-		}
-		*/
 			input = in.nextLine();//get square
 			if (verify(poop)==true){//board is good
-				find = input.split(" ");//parse second line and get piece at space
-				poop = find(Integer.parseInt(find[0]),Integer.parseInt(find[1]),poop);
-				if (poop != null){//found space
-					out.print(poop.piece.type+" ");
+				look = input.split(" ");//parse second line and get piece at space
+				helper = find(Integer.parseInt(look[0]),Integer.parseInt(look[1]),poop);
+				if (helper != null){//found space
+					out.print(helper.piece.type+" ");
 				}
 				else{
 					out.print("- ");
 				}
+			
+			int[] attacks = new int[poop.piece.boardSize*poop.piece.boardSize];
+			found = false;
+			while(poop.after != null && found != true){
+			attacks = poop.piece.attack(poop.piece);//generate attack array
+				for (i = 0; attacks[i] != 0;){//all elements in attack array
+					helper = find(attacks[i++],attacks[i++],poop);//search list for attack spaces
+					 if(helper != null && (helper.piece.shit != poop.piece.shit)){//node at piece it can attack
+					 	attacks[i] = 0;//exit loop
+					 	found = true;
+						 }
+						}
+						if (found == false) poop = poop.after;
+					}
+				if (found == true){
+					out.println(poop.piece.type+" "+poop.piece.X+" "+poop.piece.Y+" "
+					+helper.piece.type+" "+helper.piece.X+" "+helper.piece.Y);
+			poop = poop.head;
+				}
+				else out.println("-");
 			}
-			else {
+
+				else {
 				out.println("Invalid");
 			}
-			poop = poop.head;
-			/*
-			while (poop!=null){
-				System.out.println(poop.piece);
-				poop = poop.after;
-			}
-			*/
-			poop=poop.head;
-			while(poop.after!=null){
-				poop=poop.after;
-			}
-			System.out.println("last node is: "+poop.piece);
-
 		}
 		in.close();
 		out.close();
-		/*
-		int[] attacks = new int[poop.piece.boardSize*poop.piece.boardSize];
-
-		Arrays.fill(attacks, -1);
-		System.out.println(attacks[1]);
-		poop = poop.head;
-		Node helper = poop;
-		System.out.println(poop.piece);
-		queen queef = new queen();
-		queef.boardSize=6;
-		queef.X=3;
-		queef.Y = 3;
-		
-		/*
-		for(int i = 0; attacks[i]!= -1;){
-			System.out.println(attacks[i++]+" "+attacks[i++]);
-		}
-		
-	
-		input = "8 q 4 3 k 4 4 r 8 2 r 8 8 b 1 1 K 4 8 N 7 7";
-		poop = createBoard(input, poop);//make linked list of pieces
-		Node helper = poop;
-		int[] attacks = new int[poop.piece.boardSize*poop.piece.boardSize];
-		System.out.print("Bsize: "+poop.piece.boardSize);
-			System.out.print("  (x,y): "+(poop.piece.X)+","+(poop.piece.Y));
-			System.out.println("  piece: "+poop.piece);	
-		attacks = poop.piece.attack(poop.piece);//generate attack array
-		for (i=0; i<attacks.length;){
-			System.out.println("attacks: "+attacks[i++]+" "+attacks[i++]);
-		}
-		for (i = 0; attacks[i] != 0;){//all elements in attack array
-			helper = find(attacks[i++],attacks[i++],poop);//search list for attack spaces
-			 if(helper != null){//node at piece it can attack
-			 	attacks[i] = 0;//exit loop
-			 }
-			 //if
-		}
-		/*
-		poop = head;
-		while (i<=2){
-			System.out.println(i++);
-			input = in.nextLine();//get board data line
-			poop = createBoard(input, poop);//make linked list of pieces
-
-			
-			System.out.println("the board is: "+verify(poop));
-			poop = poop.head;
-			while (poop!=null){
-			System.out.print("Bsize: "+poop.piece.boardSize);
-			System.out.print("  (x,y): "+(poop.piece.X)+","+(poop.piece.Y));
-			System.out.println("  piece: "+poop.piece);
-			poop = poop.after;
-		}
-		}
-		
-		System.out.println("piece "+poop.piece.type+" at ("+poop.piece.X+","+poop.piece.Y+")"+
-			" attacks "+helper.piece.type+" at ("+helper.piece.X+","+helper.piece.Y+")");
-		*/	
 			
 	}
-
-
-
-
 
 	static Node createBoard(String firstL, Node helper) throws IOException{//returns linked list of board
 		String[] data = firstL.split(" ");//split line into characters
@@ -212,6 +147,7 @@ class chess extends Node{
 			//System.out.println("i is: "+i+"piece type is: "+helper.piece.type);
 			if(data[i]==data[i].toUpperCase()){//check if black
 				helper.piece.type = Character.toUpperCase(helper.piece.type);
+				helper.piece.shit = 'b';
 			}
 			helper.piece.X = Integer.parseInt(data[++i]);//add x place 
 			helper.piece.Y = Integer.parseInt(data[++i]);//add y place
@@ -376,173 +312,173 @@ class King extends chessPiece{
 		int[] spots = new int[piece.boardSize*piece.boardSize];
 		int i = 0;
 		
-		if (piece.X == 0){//if at left edge of board
+		if (piece.X == 1){//if at left edge of board
 			//right
-			spots[i++] = piece.Y;
-			spots[i++] = piece.X+1;
-			if (piece.Y == 0){//at top
+			spots[i++] = piece.X;
+			spots[i++] = piece.Y+1;
+			if (piece.X == 1){//at top
 				//down
-				spots[i++] = piece.Y-1;
-				spots[i++] = piece.X;
+				spots[i++] = piece.X-1;
+				spots[i++] = piece.Y;
 				//lower right diagonal
-				spots[i++] = piece.Y-1;
-				spots[i++] = piece.X+1;	
+				spots[i++] = piece.X-1;
+				spots[i++] = piece.Y+1;	
 		}
-			else if (piece.Y == piece.boardSize){//at bottom
+			else if (piece.X == piece.boardSize){//at bottom
 				//up
-				spots[i++] = piece.Y+1;
-				spots[i++] = piece.X;
-				//upper right diagonal
-				spots[i++] = piece.Y+1;
 				spots[i++] = piece.X+1;
+				spots[i++] = piece.Y;
+				//upper right diagonal
+				spots[i++] = piece.X+1;
+				spots[i++] = piece.Y+1;
 		}
 
 			else{//can do all moves not at left
 				//down
-				spots[i++] = piece.Y-1;
-				spots[i++] = piece.X;
+				spots[i++] = piece.X-1;
+				spots[i++] = piece.Y;
 				//lower right diagonal
-				spots[i++] = piece.Y-1;
-				spots[i++] = piece.X+1;	
+				spots[i++] = piece.X-1;
+				spots[i++] = piece.Y+1;	
 				//up
-				spots[i++] = piece.Y+1;
-				spots[i++] = piece.X;
-				//upper right diagonal
-				spots[i++] = piece.Y+1;
 				spots[i++] = piece.X+1;
+				spots[i++] = piece.Y;
+				//upper right diagonal
+				spots[i++] = piece.X+1;
+				spots[i++] = piece.Y+1;
 			}
 		}
 
 	else if (piece.X == piece.boardSize){//piece at right edge
 			//left
-			spots[i++] = piece.Y;
-			spots[i++] = piece.X-1;
-			if (piece.Y == 0){//at top
-				//down
-				spots[i++] = piece.Y-1;
-				spots[i++] = piece.X;
-				//lower left diagonal
-				spots[i++] = piece.Y-1;
-				spots[i++] = piece.X-1;	
-		}
-			else if (piece.Y == piece.boardSize){//at bottom
-			//up
-			spots[i++] = piece.Y+1;
 			spots[i++] = piece.X;
+			spots[i++] = piece.Y-1;
+			if (piece.X == 1){//at top
+				//down
+				spots[i++] = piece.X-1;
+				spots[i++] = piece.Y;
+				//lower left diagonal
+				spots[i++] = piece.X-1;
+				spots[i++] = piece.Y-1;	
+		}
+			else if (piece.X == piece.boardSize){//at bottom
+			//up
+			spots[i++] = piece.X+1;
+			spots[i++] = piece.Y;
 			//upper left diagonal
-			spots[i++] = piece.Y+1;
-			spots[i++] = piece.X-1;
+			spots[i++] = piece.X+1;
+			spots[i++] = piece.Y-1;
 		}
 		else{
 			//up
-			spots[i++] = piece.Y+1;
-			spots[i++] = piece.X;
+			spots[i++] = piece.X+1;
+			spots[i++] = piece.Y;
 			//upper left diagonal
-			spots[i++] = piece.Y+1;
-			spots[i++] = piece.X-1;
+			spots[i++] = piece.X+1;
+			spots[i++] = piece.Y-1;
 			//down
-			spots[i++] = piece.Y-1;
-			spots[i++] = piece.X;
+			spots[i++] = piece.X-1;
+			spots[i++] = piece.Y;
 			//lower left diagonal
-			spots[i++] = piece.Y-1;
-			spots[i++] = piece.X-1;	
+			spots[i++] = piece.X-1;
+			spots[i++] = piece.Y-1;	
 		}
 	}
-	else if (piece.Y == piece.boardSize){//at top
+	else if (piece.X == piece.boardSize){//at top
 			//down
-			spots[i++] = piece.Y-1;
-			spots[i++] = piece.X;
+			spots[i++] = piece.X-1;
+			spots[i++] = piece.Y;
 			if (piece.X == piece.boardSize){//piece at right edge
 				//left
-				spots[i++] = piece.Y;
-				spots[i++] = piece.X-1;
+				spots[i++] = piece.X;
+				spots[i++] = piece.Y-1;
 				//lower left diagonal
-				spots[i++] = piece.Y-1;
-				spots[i++] = piece.X-1;	
+				spots[i++] = piece.X-1;
+				spots[i++] = piece.Y-1;	
 			}
-			else if (piece.X == 0){//if at left edge of board
+			else if (piece.X == 1){//if at left edge of board
 				//right
-				spots[i++] = piece.Y;
-				spots[i++] = piece.X+1;
+				spots[i++] = piece.X;
+				spots[i++] = piece.Y+1;
 				//lower right diagonal
-				spots[i++] = piece.Y-1;
-				spots[i++] = piece.X+1;	
+				spots[i++] = piece.X-1;
+				spots[i++] = piece.Y+1;	
 			}
 			else {//can do all moves
 					//left
-				spots[i++] = piece.Y;
-				spots[i++] = piece.X-1;
+				spots[i++] = piece.X;
+				spots[i++] = piece.Y-1;
 				//lower left diagonal
-				spots[i++] = piece.Y-1;
-				spots[i++] = piece.X-1;	
+				spots[i++] = piece.X-1;
+				spots[i++] = piece.Y-1;	
 				//right
-				spots[i++] = piece.Y;
-				spots[i++] = piece.X+1;
+				spots[i++] = piece.X;
+				spots[i++] = piece.Y+1;
 				//lower right diagonal
-				spots[i++] = piece.Y-1;
-				spots[i++] = piece.X+1;	
+				spots[i++] = piece.X-1;
+				spots[i++] = piece.Y+1;	
 			}
 		}
-		else if (piece.Y == 0){//at bottom
+		else if (piece.X == 1){//at bottom
 			//up
-			spots[i++] = piece.Y+1;
-			spots[i++] = piece.X;
+			spots[i++] = piece.X+1;
+			spots[i++] = piece.Y;
 			if (piece.X == piece.boardSize){//piece at right edge
 				//left
-				spots[i++] = piece.Y;
-				spots[i++] = piece.X-1;
+				spots[i++] = piece.X;
+				spots[i++] = piece.Y-1;
 				//upper left diagonal
-				spots[i++] = piece.Y+1;
-				spots[i++] = piece.X-1;	
-			}
-			else if (piece.X == 0){//if at left edge of board
-				//right
-				spots[i++] = piece.Y;
 				spots[i++] = piece.X+1;
-				//upper right diagonal
+				spots[i++] = piece.Y-1;	
+			}
+			else if (piece.X == 1){//if at left edge of board
+				//right
+				spots[i++] = piece.X;
 				spots[i++] = piece.Y+1;
-				spots[i++] = piece.X+1;	
+				//upper right diagonal
+				spots[i++] = piece.X+1;
+				spots[i++] = piece.Y+1;	
 			}
 			else {//can do all moves
 				//left
-				spots[i++] = piece.Y;
-				spots[i++] = piece.X-1;
+				spots[i++] = piece.X;
+				spots[i++] = piece.Y-1;
 				//upper left diagonal
-				spots[i++] = piece.Y+1;
-				spots[i++] = piece.X-1;	
-				//right
-				spots[i++] = piece.Y;
 				spots[i++] = piece.X+1;
-				//upper right diagonal
+				spots[i++] = piece.Y-1;	
+				//right
+				spots[i++] = piece.X;
 				spots[i++] = piece.Y+1;
-				spots[i++] = piece.X+1;	
+				//upper right diagonal
+				spots[i++] = piece.X+1;
+				spots[i++] = piece.Y+1;	
 			}
 		}
 		else {//not at board edge
 			//left
-			spots[i++] = piece.Y;
-			spots[i++] = piece.X-1;
+			spots[i++] = piece.X;
+			spots[i++] = piece.Y-1;
 			//upper left diagonal
-			spots[i++] = piece.Y+1;
-			spots[i++] = piece.X-1;	
-			//right
-			spots[i++] = piece.Y;
 			spots[i++] = piece.X+1;
+			spots[i++] = piece.Y-1;	
+			//right
+			spots[i++] = piece.X;
+			spots[i++] = piece.Y+1;
 			//upper right diagonal
-			spots[i++] = piece.Y+1;
-			spots[i++] = piece.X+1;	
+			spots[i++] = piece.X+1;
+			spots[i++] = piece.Y+1;	
 			//up
-			spots[i++] = piece.Y+1;
-			spots[i++] = piece.X;
+			spots[i++] = piece.X+1;
+			spots[i++] = piece.Y;
 			//down
-			spots[i++] = piece.Y-1;
-			spots[i++] = piece.X;
+			spots[i++] = piece.X-1;
+			spots[i++] = piece.Y;
 			//lower left diagonal
-			spots[i++] = piece.Y-1;
-			spots[i++] = piece.X-1;	
+			spots[i++] = piece.X-1;
+			spots[i++] = piece.Y-1;	
 			//lower right diagonal
-			spots[i++] = piece.Y-1;
-			spots[i++] = piece.X+1;	
+			spots[i++] = piece.X-1;
+			spots[i++] = piece.Y+1;	
 
 		}
 		return spots;
--- HW2/chessPiece.java
+++ HW2/chessPiece.java
@@ -1,6 +1,7 @@
 public abstract class chessPiece{
 	int boardSize = 0;
 	char type = 'u';//stores piece type
+	char shit = 'u';
 	int X = 0;
 	int Y = 0;
 	abstract int[] attack(chessPiece piece);
