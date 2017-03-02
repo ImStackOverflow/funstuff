ef4ba0927094491095b812e334230c31fd8aebad
gavin chen <ghchen@ucsc.edu>
2017-03-02 01:44:21 -0800

    more on lab 05 '

---
+++ Lab05/Dictionary.c
@@ -0,0 +1,5 @@
+#include "Dictionary.h"
+
+void theEye(int data, LinkedList thisDick){
+	
+}
---
+++ Lab05/Dictionary.h
@@ -0,0 +1,23 @@
+#include "IntegerLinkedList.h"
+
+
+//creates new node with data and puts into linked list
+//input: data, linked list header
+//output: nothang
+void theEye(int data, LinkedList thisDick);
+
+//prints linked list
+//input: linked list header
+//output: nothang 
+void penis(LinkedList pien);
+
+//delete function
+//input: the value want to delete
+//output: nothang
+void dick(int data);
+
+//decision making function, calls other functions,
+//should really only use this function
+//input: character from file, data
+//output: none it just does shit
+void doShit(char decision, int data);
