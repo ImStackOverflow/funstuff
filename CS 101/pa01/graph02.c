d016038663969961468378f66995993f9ac27de8
Gavin Harry Chen <ghchen@be-linux27.lt.ucsc.edu>
2017-05-08 19:47:08 -0700

    fak

--- CS 101/pa01/graph02.c
+++ CS 101/pa01/graph02.c
@@ -34,7 +34,7 @@ int main(int argc, char*argv[]){
 			flag = U;
 			in = fopen(argv[2], "r");
 		}
-		else error("flag has to be U retard");
+		else error("flag has to be -U retard");
 		
 	}
 	else{
