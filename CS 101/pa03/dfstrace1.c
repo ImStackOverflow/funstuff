6f0bdad775378a60553761ac644e9277c6bf7832
gavin chen <ghchen@ucsc.edu>
2017-06-21 12:13:51 -0700

    fuck me fucke me fuck me

--- CS 101/pa03/dfstrace1.c
+++ CS 101/pa03/dfstrace1.c
@@ -234,8 +234,8 @@ Graph DFSPhase2(Graph G){
 	makeWhite(imTrans);//set all nodes to white
 	int pein, dick = intSize(imTrans->stack);
 	for(int i = dick; i >= 0; i--){
-		intVecPop(imTrans->stack);
 		pein = intTop(imTrans->stack);
+		intVecPop(imTrans->stack);
 		if (imTrans->info[pein-1][0] == WHITE){ //if node is white (undiscovered)
 		time = DFStrace2(imTrans, pein, time, pein);
 		}
