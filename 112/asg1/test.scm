e2c443bd2513eef47939266dde297450595d66ef
gavin chen <ghchen@ucsc.edu>
2017-10-10 00:05:21 -0700

    thinking and learning bout shit

--- 112/asg1/test.scm
+++ 112/asg1/test.scm
@@ -1,2 +1,14 @@
-(define nutz 
-	(let 
+(define (nutz poop) 
+	(let ((p (open-input-file poop)))
+  (let f ((x (read p)))
+    (if (eof-object? x)
+        (begin
+          (close-input-port p)
+          '())
+        (display x))))
+		)
+		
+(define *function-list* (make-hash))
+		
+
+	
