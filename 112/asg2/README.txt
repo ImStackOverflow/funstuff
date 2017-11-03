ac32e5453ecf2be5ffc4baa0ba832249f42736f1
Gavin Harry Chen <ghchen@unix1.lt.ucsc.edu>
2017-11-02 18:38:30 -0700

    ready to submit motherfucker

---
+++ 112/asg2/README.txt
@@ -0,0 +1,29 @@
+Gavin Chen
+ghchen@ucsc.edu
+1492980
+11/2/17
+
+In this assignment I implimented dc in ocaml. I was able to finish 
+all the functions provided and also the line split function and 
+the registers. I ran the test input files and the calculations 
+were correct. I also ran the mk.test script and no errors were
+produced execept for discrepancies with 
+the EOF output and some other minor differences with messages. 
+
+This was kinda fun as far as homework goes. Way better than the 
+f@!#$%$ scheme assignment. The language is better, or maybe just
+the assignement was easier. Either way I didn't want to blow 
+my brains out at then end of it and was actually able to submit 
+a working program that passed the tests. 
+
+I know no one will end up reading this except the grading script but 
+Mackey please have more assignments like this one scheme is the worst 
+thing I have ever known and the interpreter molested my brain to mush. 
+Also keep quoting Djikstra some people seem to be tired of it 
+but they're also CS losers with no sense of humor unless you 
+translate the joke into ASCII binary and feed it to them on punch 
+ccards (which I honestly would not be surprised if you knew how to do).
+
+Keep on Ballin prof you're a great teacher.
+XOXO
+-Jesus
--- 112/asg2/bigint.ml
+++ 112/asg2/bigint.ml
@@ -17,50 +17,50 @@ module Bigint = struct
     let strlen    = String.length
     let strsub    = String.sub
     let zero      = Bigint (Pos, [])
-	
-	
-	let trimZero penis = 
-	   let penis = reverse penis
-	     in let rec trimZero' vagina = 
-		    match vagina with
-			| []         -> []
-			| 0::pussy   -> trimZero' pussy
-			| dick       -> dick
-		  in reverse (trimZero' penis)
-	
-	let rec compare' x y = 
-	   if x = [] (*reached end of lists *)
-	      then 0 (* numbers equal *)
-		  else
-		  (*check numerical value *)
-		     let a = car x 
-		     and b = car y
-	         in match (a > b, a = b) with
-	               | true, false    -> 1  (* a is bigger num *)
-		           | false, false   -> 2 (*b is bigger num*)
-		           | false, true    -> compare' (cdr x) (cdr y) 
-				   | _, _           -> 3
-		  (* go further down num *)
-			     
-	
-	(* checks length of 2 lists
-	   0 if first = second
-	   1 if first > second
-	   2 if second > first *)
-	let compare x y = 
-	    let a = List.length x
-		and b = List.length y
-		(*checks first using length *)
-		in match (a > b, a = b) with
-		   | true, false    -> 1
-		   | false, false   -> 2
-		   | false, true    ->
-		   (*otherwise check numerical values *)
-		      let x = reverse x
-			  and y = reverse y 
-			  in compare' x y
-		   | _, _           -> 3
-				 
+   
+   
+   let trimZero penis = 
+      let penis = reverse penis
+        in let rec trimZero' vagina = 
+          match vagina with
+         | []         -> []
+         | 0::pussy   -> trimZero' pussy
+         | dick       -> dick
+        in reverse (trimZero' penis)
+   
+   let rec compare' x y = 
+      if x = [] (*reached end of lists *)
+         then 0 (* numbers equal *)
+        else
+        (*check numerical value *)
+           let a = car x 
+           and b = car y
+            in match (a > b, a = b) with
+                  | true, false    -> 1  (* a is bigger num *)
+                 | false, false   -> 2 (*b is bigger num*)
+                 | false, true    -> compare' (cdr x) (cdr y) 
+               | _, _           -> 3
+        (* go further down num *)
+              
+   
+   (* checks length of 2 lists
+      0 if first = second
+      1 if first > second
+      2 if second > first *)
+   let compare x y = 
+       let a = List.length x
+      and b = List.length y
+      (*checks first using length *)
+      in match (a > b, a = b) with
+         | true, false    -> 1
+         | false, false   -> 2
+         | false, true    ->
+         (*otherwise check numerical values *)
+            let x = reverse x
+           and y = reverse y 
+           in compare' x y
+         | _, _           -> 3
+             
     let charlist_of_string str = 
         let last = strlen str - 1
         in  let rec charlist pos result =
@@ -81,29 +81,29 @@ module Bigint = struct
                      then Bigint (Neg, to_intlist 1)
                      else Bigint (Pos, to_intlist 0)
 
-					 
-	let rec lineSplit value counter return 	 = 
-	   match (value, counter = 0) with 
-	   | [], _              -> return
-	   (* list is done *)
-	   | value, true    -> 
-	   (* reached 69 chars add newline and reset counter *)
-	   let front = "\\\n"
-	   in lineSplit value 69 (return @ [front])
-	   | car::cdr, false   ->
-	   (* conert list to string *)	   
-	   lineSplit cdr (counter-1) (return @ [string_of_int car])
-	   
-	   
+                
+   let rec lineSplit value counter return     = 
+      match (value, counter = 0) with 
+      | [], _              -> return
+      (* list is done *)
+      | value, true    -> 
+      (* reached 69 chars add newline and reset counter *)
+      let front = "\\\n"
+      in lineSplit value 69 (return @ [front])
+      | car::cdr, false   ->
+      (* conert list to string *)      
+      lineSplit cdr (counter-1) (return @ [string_of_int car])
+      
+      
     (*max line length = 69 *)
-	let string_of_bigint (Bigint (sign, value)) =
+   let string_of_bigint (Bigint (sign, value)) =
         match value with
         | []    -> "0"
         | value -> let reversed = reverse value
                        in strcat ""
                        ((if sign = Pos then "" else "-") ::
                         (lineSplit reversed 69 []))
-	  
+     
 
     let rec add' list1 list2 carry = match (list1, list2, carry) with
         | list1, [], 0       -> list1 
@@ -111,15 +111,15 @@ module Bigint = struct
         | list1, [], carry   -> add' list1 [carry] 0
         | [], list2, carry   -> add' [carry] list2 0
         | car1::cdr1, car2::cdr2, carry ->
-		(*if both lists have numbers to be added*)
+      (*if both lists have numbers to be added*)
           let sum = car1 + car2 + carry
-		  (*add the digits and the carry*)
+        (*add the digits and the carry*)
           in  sum mod radix :: add' cdr1 cdr2 (sum / radix)
           (* put in digit value and calculate carry with truncation *)
 
-	let rec sub2' list1 list2 carry = 
-	 (*list1 should always be longer *)
-	    match (list1, list2, carry) with
+   let rec sub2' list1 list2 carry = 
+    (*list1 should always be longer *)
+       match (list1, list2, carry) with
         | list1, [], 0       -> list1 
         | list1, [], carry   -> sub2' list1 [1] 0
         | car1::cdr1, car2::cdr2, carry ->
@@ -129,114 +129,117 @@ module Bigint = struct
           then sum + 10 :: sub2' cdr1 cdr2 1
           (* add 10 to sum and carry -1 *)
           else sum :: sub2' cdr1 cdr2 0
-          (* cons on the result *)	  
+          (* cons on the result *)     
         | [], list2, _       -> []
-		(* if list2 is longer than list1 theres an error *)		  
-		  
-	let sub' neg2 value1 value2 = 
-	    match (neg2, compare value1 value2) with
-        | Pos, 1        -> Bigint (Pos, trimZero (sub2' value1 value2 0))
-		(* a-b, a>b *)
-		| Pos, 2        -> Bigint (Neg, trimZero (sub2' value2 value1 0))
-		(* a-b, a<b *)
-        | Neg, 1        -> Bigint (Neg, trimZero (sub2' value1 value2 0))
-		(* b-a, a>b *)
-        | Neg, 2        -> Bigint (Pos, trimZero (sub2' value2 value1 0))
-		(* b-a, a<b *)
-		| _, 0          -> Bigint (Pos, [0])
-		(* numbers equal *)
-		| _, _          -> Bigint (Neg, [0])
-		
-		
-	 (* order is a - b *)
-	let sub (Bigint (neg1, value1)) (Bigint (neg2, value2)) = 
+      (* if list2 is longer than list1 theres an error *)        
+        
+   let sub' neg2 value1 value2 = 
+       match (neg2, compare value1 value2) with
+      | Pos, 1        -> Bigint (Pos, trimZero (sub2' value1 value2 0))
+      (* a-b, a>b *)
+      | Pos, 2        -> Bigint (Neg, trimZero (sub2' value2 value1 0))
+      (* a-b, a<b *)
+      | Neg, 1        -> Bigint (Neg, trimZero (sub2' value1 value2 0))
+      (* b-a, a>b *)
+      | Neg, 2        -> Bigint (Pos, trimZero (sub2' value2 value1 0))
+      (* b-a, a<b *)
+      | _, 0          -> Bigint (Pos, [0])
+      (* numbers equal *)
+      | _, _          -> Bigint (Neg, [0])
+      
+      
+    (* order is a - b *)
+   let sub (Bigint (neg1, value1)) (Bigint (neg2, value2)) = 
         if neg1 <> neg2
-		then Bigint (neg1, add' value1 value2 0)
-		else sub' neg2 value1 value2
+      then Bigint (neg1, add' value1 value2 0)
+      else sub' neg2 value1 value2
 
     let add (Bigint (neg1, value1)) (Bigint (neg2, value2)) =
-	    if neg1 = neg2 
-		then Bigint (neg1, add' value1 value2 0)
-		else match neg2 with  
-		|Pos            -> sub' Neg value1 value2
+       if neg1 = neg2 
+      then Bigint (neg1, add' value1 value2 0)
+      else match neg2 with  
+      |Pos            -> sub' Neg value1 value2
         |Neg            -> sub' Pos value1 value2
    
     let rec mul' value1 value2 acumul =
-	   match (value2, acumul) with
-	   | [], _         -> acumul
-         (* done multiplying return acumulator *)	   
-	   | car::cdr, acumul   -> 
-	      let operate = ( * ) car 
-		  (* curry multiplicator *)
-		  in let newSum = (map operate value1)
-		  (* map multiplicator to list *)
-	         and value1 = 0::value1
-			 (* shift number *)
-	         in  mul' value1 cdr (add' acumul newSum 0)
-			 (* add acumulator and new list *)
-	   
+      match (value2, acumul) with
+      | [], _         -> acumul
+         (* done multiplying return acumulator *)      
+      | car::cdr, acumul   -> 
+         let operate = ( * ) car 
+        (* curry multiplicator *)
+        in let newSum = (map operate value1)
+        (* map multiplicator to list *)
+            and value1 = 0::value1
+          (* shift number *)
+            in  mul' value1 cdr (add' acumul newSum 0)
+          (* add acumulator and new list *)
+      
    
     let mul (Bigint (neg1, value1)) (Bigint (neg2, value2)) = 
-	   if neg1 = neg2
-	   then Bigint (Pos, mul' value1 value2 [0])
-	   (*makes a positive # *)
-	   else Bigint (Neg, mul' value1 value2 [0])
-	   (*makes a negative # *)
-	    
-		
-	(*uses egyptian multiplication *)
-	let rec divrem dividend divisor adder = 
-	   if (compare divisor dividend) = 1
-	      then [0], dividend
-		  else let quotient, remainder =
-		        divrem dividend (add' divisor divisor 0) (add' adder adder 0)
-			 in if (compare remainder divisor) = 2
-			    then quotient, remainder
-				(*hit end, return result *)
-				else (add' quotient adder 0), (trimZero (sub2' remainder divisor 0))
-				(* otherwise keep adding to quotent
-				and sub divisor from remaining number *)
-	
+      if neg1 = neg2
+      then Bigint (Pos, mul' value1 value2 [0])
+      (*makes a positive # *)
+      else Bigint (Neg, mul' value1 value2 [0])
+      (*makes a negative # *)
+       
+      
+   (*uses egyptian multiplication *)
+   let rec divrem dividend divisor adder = 
+      if (compare divisor dividend) = 1
+         then [0], dividend
+        else let quotient, remainder =
+              divrem dividend 
+                     (add' divisor divisor 0) 
+                     (add' adder adder 0)
+          in if (compare remainder divisor) = 2
+             then quotient, remainder
+            (*hit end, return result *)
+            else (add' quotient adder 0), 
+                 (trimZero (sub2' remainder divisor 0))
+            (* otherwise keep adding to quotent
+            and sub divisor from remaining number *)
+   
     let div (Bigint (neg1, value1)) (Bigint (neg2, value2)) = 
-	   match (compare value1 value2) with
-	   | 2               -> zero
-	   (* trying to divide by larger num *)
-	   | 0               -> Bigint (Pos, [1])
-	   (* divide by same number *)
-	   | 1               -> 
-	   (* neg or pos nums cleared for div *)
-   	      let quotient, _ =
-	      divrem value1 value2 [1] 
-	      in if neg1 = neg2
-	         then Bigint (Pos, trimZero quotient)
-			 else Bigint (Neg, trimZero quotient)
-	   | _               -> zero
-	   (* fuck u and your warnings *)
-	   
+      match (compare value1 value2) with
+      | 2               -> zero
+      (* trying to divide by larger num *)
+      | 0               -> Bigint (Pos, [1])
+      (* divide by same number *)
+      | 1               -> 
+      (* neg or pos nums cleared for div *)
+            let quotient, _ =
+         divrem value1 value2 [1] 
+         in if neg1 = neg2
+            then Bigint (Pos, trimZero quotient)
+          else Bigint (Neg, trimZero quotient)
+      | _               -> zero
+      (* fuck u and your warnings *)
+      
     let rem (Bigint (neg1, value1)) (Bigint (neg2, value2)) = 
-	   let _, remainder = divrem value1 value2 [1]
-	   in Bigint (Pos, trimZero remainder)
+      let _, remainder = divrem value1 value2 [1]
+      in Bigint (Pos, trimZero remainder)
 
-	
-	let rec pow' value1 value2 acumul = 
-	    match value2 with
-		| []         -> acumul
-		| value2     -> 
-		  pow' value1 (* pass in base *)
-		  (trimZero (sub2' value2 [1] 0)) (* exp-- *)
-		  (mul' acumul value1 [0]) (* accumulator *)
-	
-	(* b^a *)
+   
+   let rec pow' value1 value2 acumul = 
+       match value2 with
+      | []         -> acumul
+      | value2     -> 
+        pow' value1 (* pass in base *)
+        (trimZero (sub2' value2 [1] 0)) (* exp-- *)
+        (mul' acumul value1 [0]) (* accumulator *)
+   
+   (* b^a *)
     let pow (Bigint (neg1, value1)) (Bigint (neg2, value2)) = 
-	   match (neg1, neg2, ((car value2) mod 2) = 0) with 
-	   | _, Neg, _             -> zero
-	   (* negative exponent fuck it *)
-	   |Pos, _, _              -> Bigint (Pos, pow' value1 value2 [1])
-	   (* positive base, othershit dont matter *)
-	   |Neg, _, true           -> Bigint (Pos, pow' value1 value2 [1])
-	   (* negative base but even exponent *)
-	   |Neg, _, false          -> Bigint (Neg, pow' value1 value2 [1])
-	   (* negative base odd exponent *)
+      match (neg1, neg2, ((car value2) mod 2) = 0) with 
+      | _, Neg, _             -> zero
+      (* negative exponent fuck it *)
+      |Pos, _, _              -> Bigint (Pos, pow' value1 value2 [1])
+      (* positive base, othershit dont matter *)
+      |Neg, _, true           -> Bigint (Pos, pow' value1 value2 [1])
+      (* negative base but even exponent *)
+      |Neg, _, false          -> Bigint (Neg, pow' value1 value2 [1])
+      (* negative base odd exponent *)
 
 end
 
